99ccbafeac55c333da0a18746d9fddfd801c5397
suchov <artem.suchov@gmail.com>
2019-03-01 22:45:36 +0200

    add logo file and style it

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Tilt from 'react-tilt';
-import './Logo.css'
+import brain from './brain.png';
+import './Logo.css';
 
 const Logo = () => {
   return (
     <div className='ma4 mt0 '>
-      <Tilt className="Tilt br2 shadow-2" options={{ max: 25 }} style={{ height: 150, width: 150 }} >
-        <div className="Tilt-inner"> 👽 </div>
+      <Tilt className="Tilt br2 shadow-2" options={{ max: 50 }} style={{ height: 150, width: 150 }} >
+        <div className="Tilt-inner pa3">
+          <img alt='logo' src={brain}/>
+        </div>
       </Tilt>
     </div>
   )
