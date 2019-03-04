2dc125826a7ad659ce1b27300a70eb5093119925
suchov <artem.suchov@gmail.com>
2019-03-04 18:24:10 +0200

    add styling, nav, logo, rank, imagelinkform

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,7 +5,7 @@ import './Logo.css';
 
 const Logo = () => {
   return (
-    <div className='ma4 mt0 '>
+    <div className='ma4 mt0'>
       <Tilt className="Tilt br2 shadow-2" options={{ max: 50 }} style={{ height: 150, width: 150 }} >
         <div className="Tilt-inner pa3">
           <img alt='logo' src={brain}/>
